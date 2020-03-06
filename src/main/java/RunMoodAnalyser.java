ff0d7dfb7df3342d045cf64037b149a9df24291c
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-06 15:41:05 +0530

    ADDES: File to create object of MoodAnalyser class.

--- src/main/java/RunMoodAnalyser.java
+++ src/main/java/RunMoodAnalyser.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class RunMoodAnalyser {
+    public static void main(String[] args) {
+        MoodAnalyzer mood=new MoodAnalyzer();
+        System.out.println("Enter your mood: ");
+        Scanner sc =new Scanner(System.in);
+        String yourMood = sc.nextLine();
+        System.out.println(mood.moodAnalyser(yourMood));
+    }
 }
