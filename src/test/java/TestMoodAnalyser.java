5c2a36d6c42f68839c1a4676124df466aff6ec29
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-06 15:54:56 +0530

    ADDED: File to validate test cases on mood analyser.

--- src/test/java/TestMoodAnalyser.java
+++ src/test/java/TestMoodAnalyser.java
@@ -1,2 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMoodAnalyser {
+    MoodAnalyzer analyzer = new MoodAnalyzer();
+    @Test
+    public void moodAnalyser_WhenMessageIncludesSad_ShouldReturnSadMood() {
+        String result = analyzer.moodAnalyser("I am in Sad Mood");
+        Assert.assertEquals("SAD", result);
+    }
 }
