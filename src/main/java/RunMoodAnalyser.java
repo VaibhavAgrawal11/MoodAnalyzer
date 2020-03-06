8b75383027f5c701c00c43640184ff382f40dee4
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-06 16:34:22 +0530

    REFACTORED: Modified changes according channges done in moodanalyser class.

--- src/main/java/RunMoodAnalyser.java
+++ src/main/java/RunMoodAnalyser.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class RunMoodAnalyser {
     public static void main(String[] args) {
-        MoodAnalyzer mood=new MoodAnalyzer();
         System.out.println("Enter your mood: ");
         Scanner sc =new Scanner(System.in);
         String yourMood = sc.nextLine();
-        System.out.println(mood.moodAnalyser(yourMood));
+        MoodAnalyzer mood=new MoodAnalyzer(yourMood);
+        System.out.println(mood.moodAnalyser());
     }
 }
