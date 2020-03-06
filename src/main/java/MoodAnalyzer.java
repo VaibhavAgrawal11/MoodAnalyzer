232596955ae47f74d3c7116f38e11d922e1bf1d4
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-06 17:12:53 +0530

    MODIFIED: NullPointerException handled.

--- src/main/java/MoodAnalyzer.java
+++ src/main/java/MoodAnalyzer.java
@@ -1,17 +1,23 @@
 public class MoodAnalyzer {
     private String message;
-    public MoodAnalyzer( String message) {
+
+    public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    public String analyseMood(String message){
-        this.message=message;
+    public String analyseMood(String message) {
+        this.message = message;
         return moodAnalyser();
     }
+
     public String moodAnalyser() {
-        if(message.contains("Sad"))
-            return "SAD";
-        else
+        try {
+            if (message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
