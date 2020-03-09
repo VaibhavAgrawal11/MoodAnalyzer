1b3d5e66aead60ffa6181f16dabd68f1467b0b1d
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-09 14:43:35 +0530

    ADDED: Method to test null pointer exception.

--- src/test/java/TestMoodAnalyser.java
+++ src/test/java/TestMoodAnalyser.java
@@ -1,17 +1,32 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 public class TestMoodAnalyser {
+    public MoodAnalyzer analyzer;
     @Test
-    public void moodAnalyser_WhenMessageIncludesSad_ShouldReturnSadMood() {
-        MoodAnalyzer analyzer = new MoodAnalyzer("I am in Sad Mood");
+    public void moodAnalyser_WhenMessageIncludesSad_ShouldReturnSadMood() throws MoodAnalysisException {
+        analyzer = new MoodAnalyzer("I am in Sad Mood");
         String result = analyzer.analyseMood();
         Assert.assertEquals("SAD", result);
     }
+
     @Test
     public void moodAnalyser_WhenMessageNotIncludesSad_ShouldReturnHappy() {
-        MoodAnalyzer analyzer = new MoodAnalyzer(null);
-        String result = analyzer.analyseMood();
+        analyzer = new MoodAnalyzer("I am in Any Mood");
+        String result = null;
+        try {
+            result = analyzer.analyseMood();
+        } catch (MoodAnalysisException e) {
+        }
         Assert.assertEquals("HAPPY", result);
+    }
+
+    @Test
+    public void moodAnalyser_WhenMessageNull_ShouldThrowException() throws MoodAnalysisException {
+        try {
+            analyzer = new MoodAnalyzer(null);
+            String result = analyzer.analyseMood();
+        }catch( MoodAnalysisException e){
+            Assert.assertEquals("Please enter valid mood",e.getMessage());
+        }
     }
 }
