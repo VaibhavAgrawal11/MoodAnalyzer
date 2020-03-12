3f487b91a181667ae40cf51409ca8b9cd8d32442
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-03-12 13:01:32 +0530

    MODIFIED: Change in  method parameters.

--- src/main/java/MoodAnalyzerFactory.java
+++ src/main/java/MoodAnalyzerFactory.java
@@ -3,9 +3,15 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MoodAnalyzerFactory {
-    public static Constructor<?> getConstructor(Class<?> ... param) throws MoodAnalysisException, ClassNotFoundException, NoSuchMethodException {
-        Class<?> moodAnalysisClass = Class.forName("MoodAnalyzer");
-        return moodAnalysisClass.getConstructor(param);
+    public static Constructor<?> getConstructor(String moodAnalyzer, Class<?> ... param) throws MoodAnalysisException {
+        try {
+            Class<?> moodAnalysisClass = Class.forName(moodAnalyzer);
+            return moodAnalysisClass.getConstructor(param);
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalysisException(e.getMessage(), MoodAnalysisException.ExceptionType.NO_SUCH_CLASS);
+        } catch (NoSuchMethodException e) {
+            throw new MoodAnalysisException(e.getMessage(), MoodAnalysisException.ExceptionType.NO_SUCH_METHOD);
+        }
     }
 
     public static Object createMoodAnalyzer(Constructor<?> constructor) throws IllegalAccessException, InvocationTargetException, InstantiationException {
